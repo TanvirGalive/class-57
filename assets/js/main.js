dbc4f687c82cadaec163d365412378e77007161a
TanvirGalive <galivetanvir@gmail.com>
2017-06-18 04:00:59 +0600

    this is initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $(".testimonial-author-title").perfectScrollbar();
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
